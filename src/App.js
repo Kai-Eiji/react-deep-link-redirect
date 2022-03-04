f5eebf5040e51a9197663a7b44969d68e7184a06
Kai-Eiji <kaiburkholder.jp@gmail.com>
2022-03-03 20:33:19 -0800

    parse params

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSearchParams } from "react-router-dom";
 
 const appScheme = "exp://192.168.1.87:19000/--/signup?company=Test Inc&manager=Kai Burkholder";
 const appName = 'todoList';
 
 function App(props) {
-  useEffect(()=>{
+  let [searchParams, setSearchParams] = useSearchParams();
 
+  useEffect(()=>{
     console.log(window.location.href)
   },[])
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // The serialize function here would be responsible for
+    // creating an object of { key: value } pairs from the
+    // fields in the form that make up the query.
+    let params = serializeFormQuery(event.target);
+    setSearchParams(params);
+    console.log(params);
+  }
+
 
   const isIos = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
   window.location.href = appScheme;
